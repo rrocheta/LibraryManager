72b9255cb61f3e788f4416f6b4424667e15a51c6
Ricardo Rocheta <ricardo.rocheta@cheto.eu>
2025-07-23 09:24:11 +0100

    added static data

--- backend/LibraryManager.API/Data/StaticData.cs
+++ backend/LibraryManager.API/Data/StaticData.cs
@@ -1,6 +1,21 @@
-﻿namespace LibraryManager.API.Data
+﻿using LibraryManager.API.Models;
+
+namespace LibraryManager.API.Data
 {
-    public class StaticData
+    public static class StaticData
     {
+        public static readonly List<Author> Authors = new()
+        {
+            new Author { Id = 1, Name = "Fernando Pessoa" },
+            new Author { Id = 2, Name = "Hernandes Dias lopes" },
+            new Author { Id = 3, Name = "Reinhard Bonnke" }
+        };
+
+        public static readonly List<Publisher> Publishers = new()
+        {
+            new Publisher { Id = 1, Name = "Porto Editora" },
+            new Publisher { Id = 2, Name = "Hagnos" },
+            new Publisher { Id = 3, Name = "CPAD" }
+        };
     }
 }
